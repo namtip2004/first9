2e5bb66dfb1db116c59998467e5b0044e4ae7575
namtip2004 <164287347+namtip2004@users.noreply.github.com>
2025-07-07 21:36:16 +0700

    navbar

--- NiceAdmin/connect_db.php
+++ NiceAdmin/connect_db.php
@@ -1,7 +1,7 @@
 
 <?php
 //mysqli("server name","username","password","database name")
-$conn = new mysqli("127.0.0.1", "root","","product_system");
+$conn = new mysqli("127.0.0.1", "root","","booking_massage");
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 } 
---
+++ NiceAdmin/slidebar.php
@@ -0,0 +1,83 @@
+  <!-- ======= Sidebar ======= -->
+  <aside id="sidebar" class="sidebar">
+
+    <ul class="sidebar-nav" id="sidebar-nav">
+
+      <li class="nav-item">
+        <a class="nav-link " href="index.html">
+          <i class="bi bi-grid"></i>
+          <span>Dashboard</span>
+        </a>
+      </li><!-- End Dashboard Nav -->
+      <li class="nav-item">
+        <a class="nav-link collapsed" data-bs-target="#forms-nav" data-bs-toggle="collapse" href="#">
+          <i class="bi bi-journal-text"></i><span>Forms</span><i class="bi bi-chevron-down ms-auto"></i>
+        </a>
+        <ul id="forms-nav" class="nav-content collapse " data-bs-parent="#sidebar-nav">
+          <li>
+            <a href="forms-layouts.php">
+              <i class="bi bi-circle"></i><span>ฟอร์มสมัครสมาชิก</span>
+            </a>
+          </li>
+          <a href="shopping-cart.php">
+            <i class="bi bi-circle"></i><span>สั่งสินค้า</span>
+          </a>
+        </li>
+          <li>
+            <a href="forms-product.php">
+              <i class="bi bi-circle"></i><span>ฟอร์มสินค้า</span>
+            </a>
+          </li>
+        </ul>
+      </li><!-- End Forms Nav -->
+
+      <li class="nav-item">
+        <a class="nav-link collapsed" data-bs-target="#tables-nav" data-bs-toggle="collapse" href="#">
+          <i class="bi bi-layout-text-window-reverse"></i><span>Tables</span><i class="bi bi-chevron-down ms-auto"></i>
+        </a>
+        <ul id="tables-nav" class="nav-content collapse " data-bs-parent="#sidebar-nav">
+          <li>
+            <a href="tables-data-customer.php">
+              <i class="bi bi-circle"></i><span>ข้อมูลลูกค้า</span>
+            </a>
+          </li>
+          <li>
+            <a href="tables-data-orders.php">
+              <i class="bi bi-circle"></i><span>ข้อมูลคำสั่งซื้อ</span>
+            </a>
+          </li>
+        </li>
+        <li>
+          <a href="tables-orders-complete.php">
+            <i class="bi bi-circle"></i><span>ประวัติคำสั่งซื้อ</span>
+        </a>
+        </li>
+        </ul>
+      </li><!-- End Tables Nav -->
+
+      <li class="nav-heading">Pages</li>
+
+      <li class="nav-item">
+        <a class="nav-link collapsed" href="users-profile.php">
+          <i class="bi bi-person"></i>
+          <span>Profile</span>
+        </a>
+      </li><!-- End Profile Page Nav -->
+
+      <li class="nav-item">
+        <a class="nav-link collapsed" href="pages-contact.php">
+          <i class="bi bi-envelope"></i>
+          <span>Contact</span>
+        </a>
+      </li><!-- End Contact Page Nav -->
+
+      <li class="nav-item">
+        <a class="nav-link collapsed" href="pages-login.php">
+          <i class="bi bi-box-arrow-in-right"></i>
+          <span>Login</span>
+        </a>
+      </li><!-- End Login Page Nav -->
+      
+    </ul>
+
+  </aside><!-- End Sidebar-->

2a89b784685e7b7a44fd24935d4ff94907bd9548
namtip2004 <164287347+namtip2004@users.noreply.github.com>
2025-07-29 13:10:22 +0700

    7/29/25
    ggggg

---
+++ headerUser.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- Favicons -->
+    <link href="assets/img/Logo1.png" rel="icon">
+    <link href="assets/img/apple-touch-icon.png" rel="apple-touch-icon">
+    <!-- Google Fonts -->
+    <link href="https://fonts.gstatic.com" rel="preconnect">
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Nunito:300,300i,400,400i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
+    <!-- Vendor CSS Files -->
+    <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+     <!-- CSS File -->
+  <link href="assets/css/index.css" rel="stylesheet">
+</head>
+<body>
+    <nav class="navbar-custom">
+        <a class="navbar-brand" href="#">
+          <img src="assets/img/logo-mini.png" alt="Logo" class="logo-img">
+        </a>
+        <ul class="nav-links">
+            <li class="nav-item"><a class="nav-link" href="#home">Home</a></li>
+            <li class="nav-item"><a class="nav-link" href="#massage">Massage</a></li>
+            <li class="nav-item"><a class="nav-link" href="#booking">Booking</a></li>
+            <li class="nav-item"><a class="nav-link" href="#promotion">Promotion</a></li>
+            <li class="nav-item"><a class="nav-link" href="#contact">Contact</a></li>
+        </ul>
+        <div class="profile-icon"></div>
+    </nav>
+    <!-- Vendor JS Files -->
+    <script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+</body>
+</html>
---
+++ indexuser.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="en">
+<body>
+<?php include 'headerUser.php'; ?>
+
+
+
+<?php include 'footerUser.php'; ?>
+</body>
+</html>
